df048f383fa6d5662d78236b7bfcc99d371c8ff1
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-07-17 19:14:17 +0530

    update

--- Program_1.php
+++ Program_1.php
@@ -1,18 +1,18 @@
 <?php
 echo "<h2>Collage Result<br></h2> ";
 echo "================================================= <br> " ;
-echo "Student Name : Manish<br> ";
+echo "Student Name : Sandip Limbasiya<br> ";
 echo "Course :BCA <br> ";
-echo " Enrollment no : 7361 <br>";
+echo " Enrollment no : 7173 <br>";
 echo  "  Class : C <br> ";
 echo " ================================================= <br> ";
 echo "python :- A+  <br> ";
-echo "Java :- A+ <br> ";
+echo "Java :- A <br> ";
 echo "c# :- A <br> ";
 echo "os :- A+ <br> ";
-echo "cn :- A+ <br> ";
+echo "cn :- B+ <br> ";
 echo "=================================================== <br> ";
-echo " sgpa : 8.6 <br> ";
-echo " cgpa : 8.8  <br> ";
+echo " sgpa : 6.44 <br> ";
+echo " cgpa : 7.28  <br> ";
 echo "===================================================== <br> ";
 ?>
